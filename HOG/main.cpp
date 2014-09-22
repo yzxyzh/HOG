8012157c9baa2f347d81f02a77056e143db9fb7b
yzxyzh <shuxuejinsai@126.com>
2014-09-22 11:20:10 +0800

    Initial Commit

---
+++ HOG/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HOG
+//
+//  Created by Yanzixu on 14-9-22.
+//  Copyright (c) 2014年 Yanzixu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
